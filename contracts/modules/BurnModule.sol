633da82275472873f39a8cfb16ee7d5a3bdf7092
Ryan Sauge <ryan.sauge@protonmail.com>
2022-09-27 10:47:02 +0200

    Update the version of solidity to 0.8.4 for the contracts in contracts/modules + replace the library solium by its new version ethlint

--- contracts/modules/BurnModule.sol
+++ contracts/modules/BurnModule.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MPL-2.0
 
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.4;
 
 import "../../openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 
