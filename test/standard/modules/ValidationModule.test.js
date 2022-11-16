cbb5b9abb2b410b6d281663c8a427973d6b95f88
Ryan Sauge <ryan.sauge@protonmail.com>
2022-11-16 17:14:58 +0100

    test: improve the tests of ValidationModule

--- test/standard/modules/ValidationModule.test.js
+++ test/standard/modules/ValidationModule.test.js
@@ -1,19 +1,14 @@
-const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers')
-const { DEFAULT_ADMIN_ROLE } = require('../../utils')
-const { should } = require('chai').should()
-
 const CMTAT = artifacts.require('CMTAT')
-const RuleEngineMock = artifacts.require('RuleEngineMock')
 const ValidationModuleCommon = require('../../common/ValidationModuleCommon')
 
 contract(
-  'ValidationModule',
-  function ([_, owner, address1, address2, address3, fakeRuleEngine]) {
+  'Standard - ValidationModule',
+  function ([_, admin, address1, address2, address3, fakeRuleEngine]) {
     beforeEach(async function () {
-      this.cmtat = await CMTAT.new(_, { from: owner })
-      await this.cmtat.initialize(owner, 'CMTA Token', 'CMTAT', 'CMTAT_ISIN', 'https://cmta.ch', { from: owner })
+      this.cmtat = await CMTAT.new(_, { from: admin })
+      await this.cmtat.initialize(admin, 'CMTA Token', 'CMTAT', 'CMTAT_ISIN', 'https://cmta.ch', { from: admin })
     })
 
-    ValidationModuleCommon(owner, address1, address2, address3, fakeRuleEngine)
+    ValidationModuleCommon(admin, address1, address2, address3, fakeRuleEngine)
   }
 )
