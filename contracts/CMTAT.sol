9fd7a9b9aa8a2b1be952c0bd879db9c0b1683f1a
Ryan Sauge <ryan.sauge@protonmail.com>
2023-01-05 14:11:53 +0100

    TransferAdminship: change owner for admin + add comment

--- contracts/CMTAT.sol
+++ contracts/CMTAT.sol
@@ -31,12 +31,12 @@ contract CMTAT is
     ERC20BaseModule
 {
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor(address forwarder, bool deployedWithProxy_, address owner, string memory name, string memory symbol, string memory tokenId, string memory terms
+    constructor(address forwarder, bool deployedWithProxy_, address admin, string memory name, string memory symbol, string memory tokenId, string memory terms
     ) MetaTxModule(forwarder) {
          if(!deployedWithProxy_){
             // Initialize the contract to avoid front-running
             // Warning : do not initialize the proxy
-            initialize(deployedWithProxy_, owner, name, symbol,tokenId, terms);
+            initialize(deployedWithProxy_, admin, name, symbol,tokenId, terms);
          }else{
             // Initialize the variable for the implementation
             deployedWithProxy = true;
@@ -47,13 +47,13 @@ contract CMTAT is
 
     function initialize(
         bool deployedWithProxy_,
-        address owner,
+        address admin,
         string memory name,
         string memory symbol,
         string memory tokenId,
         string memory terms
     ) public initializer {
-        __CMTAT_init(deployedWithProxy_, owner, name, symbol, tokenId, terms);
+        __CMTAT_init(deployedWithProxy_, admin, name, symbol, tokenId, terms);
     }
 
     /**
@@ -64,7 +64,7 @@ contract CMTAT is
      */
     function __CMTAT_init(
         bool deployedWithProxy_,
-        address owner,
+        address admin,
         string memory name,
         string memory symbol,
         string memory tokenId,
@@ -103,39 +103,50 @@ contract CMTAT is
         __Base_init_unchained(tokenId, terms);
 
          /* own function */
-        __CMTAT_init_unchained(deployedWithProxy_, owner);
+        __CMTAT_init_unchained(deployedWithProxy_, admin);
     }
 
 
-    function __CMTAT_init_unchained(bool deployedWithProxy_, address owner) internal onlyInitializing {
+    function __CMTAT_init_unchained(bool deployedWithProxy_, address admin) internal onlyInitializing {
         deployedWithProxy = deployedWithProxy_;
-        _grantRole(DEFAULT_ADMIN_ROLE, owner);
-        _grantRole(ENFORCER_ROLE, owner);
-        _grantRole(MINTER_ROLE, owner);
-        _grantRole(BURNER_ROLE, owner);
-        _grantRole(PAUSER_ROLE, owner);
-        _grantRole(SNAPSHOOTER_ROLE, owner);
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(ENFORCER_ROLE, admin);
+        _grantRole(MINTER_ROLE, admin);
+        _grantRole(BURNER_ROLE, admin);
+        _grantRole(PAUSER_ROLE, admin);
+        _grantRole(SNAPSHOOTER_ROLE, admin);
     }
 
+    /*
+    @notice Transfers adminship from one address to another
+    The newAdmin will have the same roles as the current admin.
+    Warning: make sure the address of newAdmin is correct.
+    By transfering his rights, the former admin loses them all.
+    */
     function transferAdminship(address newAdmin) public onlyRole(DEFAULT_ADMIN_ROLE) {
         address sender = _msgSender();
         grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
+        // EnforcementModule
         if(hasRole(ENFORCER_ROLE, sender)){
             grantRole(ENFORCER_ROLE, newAdmin);
             renounceRole(ENFORCER_ROLE, sender);
         }
+        // MintModule
         if(hasRole(MINTER_ROLE, sender)){
             grantRole(MINTER_ROLE, newAdmin);
             renounceRole(MINTER_ROLE, sender);
         }
+        // BurnModule
         if(hasRole(BURNER_ROLE, sender)){
             grantRole(BURNER_ROLE, newAdmin);
             renounceRole(BURNER_ROLE, sender);
         }
+        // PauseModule
         if(hasRole(PAUSER_ROLE, sender)){
             grantRole(PAUSER_ROLE, newAdmin);
             renounceRole(PAUSER_ROLE, sender);
         }
+        // SnapshotModule
         if(hasRole(SNAPSHOOTER_ROLE, sender)){
             grantRole(SNAPSHOOTER_ROLE, newAdmin);
             renounceRole(SNAPSHOOTER_ROLE, sender);
