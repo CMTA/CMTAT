4cb6320ee4704addfc5ca8f616b6162d98e68d9a
Diego <diego.gm@pm.me>
2023-05-31 17:35:27 +0200

    Add github CI workflow (#189)

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -4,13 +4,14 @@ const CMTAT_PROXY = artifacts.require("CMTAT_PROXY")
 const { deployProxy } = require("@openzeppelin/truffle-upgrades")
 const { Address } = require("ethereumjs-util")
 
-module.exports = async function (deployer) {
-  const flag = 0
+module.exports = async function (deployer, _network, account) {
+	const admin = process.env.ADMIN_ADDRESS ? process.env.ADMIN_ADDRESS : account[0]
+	const flag = 0
 	const ZERO_ADDRESS = Address.zero().toString()
-  const proxyContract = await deployProxy(
-    CMTAT_PROXY,
+	const proxyContract = await deployProxy(
+		CMTAT_PROXY,
 		[
-			process.env.ADMIN_ADDRESS,
+			admin,
 			"Test CMTA Token",
 			"TCMTAT",
 			"TCMTAT_ISIN",
@@ -23,7 +24,7 @@ module.exports = async function (deployer) {
 			deployer,
 			constructorArgs: [ZERO_ADDRESS]
 		}
-  );
+	);
 	await CMTAT_PROXY.deployed()
-  console.log("Proxy deployed at: ", proxyContract.address)
+	console.log("Proxy deployed at: ", proxyContract.address)
 }
