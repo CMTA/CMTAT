28301d07a35c662d31ed72bdddc8dce3c2dc3fd8
Ryan Sauge <ryan.sauge@protonmail.com>
2023-09-12 13:41:04 +0200

    AuthorizationModule: improve test

--- test/common/AuthorizationModule/AuthorizationModuleCommon.js
+++ test/common/AuthorizationModule/AuthorizationModuleCommon.js
@@ -42,6 +42,9 @@ function AuthorizationModuleCommon (admin, address1, address2) {
       })
     })
 
+    /*
+    Already tested by OpenZeppelin library
+    */
     it('testCannotNonAdminGrantRole', async function () {
       // Arrange - Assert
       (await this.cmtat.hasRole(PAUSER_ROLE, address1)).should.equal(false)
@@ -55,6 +58,9 @@ function AuthorizationModuleCommon (admin, address1, address2) {
       (await this.cmtat.hasRole(PAUSER_ROLE, address1)).should.equal(false)
     })
 
+    /*
+    Already tested by OpenZeppelin library
+    */
     it('testCannotNonAdminRevokeRole', async function () {
       // Arrange
       (await this.cmtat.hasRole(PAUSER_ROLE, address1)).should.equal(false)
@@ -71,6 +77,9 @@ function AuthorizationModuleCommon (admin, address1, address2) {
       (await this.cmtat.hasRole(PAUSER_ROLE, address1)).should.equal(true)
     })
 
+    /*
+    Already tested by OpenZeppelin library
+    */
     it('testCanTransferAdminship', async function () {
       // Arrange
       (await this.cmtat.hasRole(PAUSER_ROLE, address1)).should.equal(false)
@@ -86,6 +95,7 @@ function AuthorizationModuleCommon (admin, address1, address2) {
       // Assert
       (await this.cmtat.hasRole(PAUSER_ROLE, address1)).should.equal(true)
     })
+
 
     it('testCanAdminTransferAdminship', async function () {
       // Arrange - Assert
@@ -105,6 +115,22 @@ function AuthorizationModuleCommon (admin, address1, address2) {
       (await this.cmtat.owner()).should.equal(address1)
     });
 
+    /*
+    Already tested by OpenZeppelin library
+    */
+    it('testCannotNonAdminTransferAdminship', async function () {
+      // Arrange - Assert
+      (await this.cmtat.owner()).should.equal(admin)
+      // Starts an admin transfer
+      await expectRevertCustomError(
+        this.cmtat.beginDefaultAdminTransfer(address1, { from: address1 }),
+        'AccessControlUnauthorizedAccount',
+        [address1, DEFAULT_ADMIN_ROLE]
+      );
+      // Assert
+      (await this.cmtat.owner()).should.equal(admin)
+    });
+
     it('testCanAdminTransferAdminshipDirectly', async function () {
       // Arrange - Assert
       (await this.cmtat.owner()).should.equal(admin)
@@ -117,8 +143,7 @@ function AuthorizationModuleCommon (admin, address1, address2) {
       (await this.cmtat.owner()).should.equal(address1)
     });
 
-
-    it('testCannnotNonAdminTransferAdminshipDirectly', async function () {
+    it('testCannotNonAdminTransferAdminshipDirectly', async function () {
       // Arrange - Assert
       (await this.cmtat.owner()).should.equal(admin)
       // Transfer the rights
