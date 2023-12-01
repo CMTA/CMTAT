7f3e7dc8207dcb8139045c3cc5b7db363d2ca284
Daniel Zarifpour <z@rifpour.xyz>
2023-12-01 01:20:18 -0500

    refactor: 🪖 migrate from truffle to hardhat
    - Remove truffle-config.js
    - Remove migrations/1_deploy_contracts.js
    - Add scripts/1_deploy_contracts.js
    - Update hardhat.config.js with network configurations
    - Update ESLint configuration, environment, and globals
    - Update devDependencies in package.json
    - Update deployment.test.js with new deployment logic
    - Update .env.example with private key and node URLs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,43 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-require('@nomiclabs/hardhat-truffle5')
+require('dotenv').config()
+require('hardhat-gas-reporter')
 require('solidity-coverage')
 require('solidity-docgen')
+require('@openzeppelin/hardhat-upgrades')
+require('@nomiclabs/hardhat-etherscan')
+require('@nomicfoundation/hardhat-chai-matchers')
+
 module.exports = {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+      chainId: 1
+    },
+    ganache: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 1337
+    },
+    live: {
+      url: 'http://178.25.19.88:80',
+      chainId: 1
+      // Add other optional config values here
+    },
+    goerli: {
+      url: process.env.GOERLI_NODE,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5
+    },
+    polygon: {
+      url: process.env.POLYGON_NODE,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 137,
+      gasPrice: 450000000000
+    },
+    mainnet: {
+      url: process.env.MAINNET_NODE,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 1
+    }
+  },
   solidity: {
     version: '0.8.17',
     settings: {
@@ -11,5 +46,18 @@ module.exports = {
         runs: 200
       }
     }
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      polygon: process.env.POLYGONSCAN_API_KEY,
+      goerli: process.env.ETHERSCAN_API_KEY
+    }
+  },
+  gasReporter: {
+    // eslint-disable-next-line no-unneeded-ternary
+    enabled: (process.env.REPORT_GAS) ? true : false,
+    outputFile: 'gas-report.txt',
+    noColors: true
   }
 }
--- test/proxy/modules/AuthorizationModule.test.js
+++ test/proxy/modules/AuthorizationModule.test.js
@@ -1,15 +1,84 @@
-const CMTAT = artifacts.require('CMTAT_BASE')
-const { deployProxy } = require('@openzeppelin/truffle-upgrades')
-const AuthorizationModuleCommon = require('../../common/AuthorizationModule/AuthorizationModuleCommon')
+const { expect } = require('chai')
+const { ethers, upgrades } = require('hardhat')
+const { DEFAULT_ADMIN_ROLE } = require('../../utils')
+const { PAUSER_ROLE } = require('../../utils')
 
-contract(
-  'Proxy - AuthorizationModule',
-  function ([_, owner, address1, address2]) {
-    beforeEach(async function () {
-      this.flag = 5
-      this.cmtat = await deployProxy(CMTAT, [owner, 'CMTA Token', 'CMTAT', 18, 'CMTAT_ISIN', 'https://cmta.ch', 'CMTAT_info', this.flag])
+describe('Proxy - AuthorizationModule', function () {
+  let owner, address1, address2, cmtat
+
+  beforeEach(async function () {
+    [owner, address1, address2] = await ethers.getSigners()
+
+    const flag = 5
+    const CMTAT_BASE = await ethers.getContractFactory('CMTAT_BASE')
+    cmtat = await upgrades.deployProxy(CMTAT_BASE, [owner.address, 'CMTA Token', 'CMTAT', 18, 'CMTAT_ISIN', 'https://cmta.ch', 'CMTAT_info', flag], { initializer: 'initialize' })
+  })
+
+  context('Authorization', function () {
+    it('testAdminCanGrantRole', async function () {
+      await hasRole(DEFAULT_ADMIN_ROLE, owner.address, true)
+      await hasRole(PAUSER_ROLE, address1.address, false)
+      await grantRole(owner, PAUSER_ROLE, address1.address)
+      await hasRole(PAUSER_ROLE, address1.address, true)
     })
 
-    AuthorizationModuleCommon(owner, address1, address2)
-  }
-)
+    it('testAdminCanRevokeRole', async function () {
+      await grantRole(owner, PAUSER_ROLE, address1.address)
+      await hasRole(PAUSER_ROLE, address1.address, true)
+      await revokeRole(owner, PAUSER_ROLE, address1.address)
+      await hasRole(PAUSER_ROLE, address1.address, false)
+    })
+
+    it('testOnlyRoleCanRenounceRole', async function () {
+      await grantRole(owner, PAUSER_ROLE, address1.address)
+      await hasRole(PAUSER_ROLE, address1.address, true)
+      await renounceRole(address1, PAUSER_ROLE, address1.address)
+      await hasRole(PAUSER_ROLE, address1.address, false)
+    })
+
+    it('testCannotNonAdminGrantRole', async function () {
+      await hasRole(PAUSER_ROLE, address1.address, false)
+      await expect(
+        cmtat.connect(address2).grantRole(PAUSER_ROLE, address1.address)
+      ).to.be.revertedWith('AccessControl: account ' + address2.address.toLowerCase() + ' is missing role ' + DEFAULT_ADMIN_ROLE)
+      await hasRole(PAUSER_ROLE, address1.address, false)
+    })
+
+    it('testCannotNonAdminRevokeRole', async function () {
+      await hasRole(PAUSER_ROLE, address1.address, false)
+      await grantRole(owner, PAUSER_ROLE, address1.address)
+      await hasRole(PAUSER_ROLE, address1.address, true)
+      await expect(
+        cmtat.connect(address2).revokeRole(PAUSER_ROLE, address1.address)
+      ).to.be.revertedWith('AccessControl: account ' + address2.address.toLowerCase() + ' is missing role ' + DEFAULT_ADMIN_ROLE)
+      await hasRole(PAUSER_ROLE, address1.address, true)
+    })
+
+    it('testCannotRenounceToNotYourRole', async function () {
+      await hasRole(PAUSER_ROLE, address1.address, false)
+      await grantRole(owner, PAUSER_ROLE, address1.address)
+      await hasRole(PAUSER_ROLE, address1.address, true)
+      await expect(
+        cmtat.connect(address2).renounceRole(PAUSER_ROLE, address1.address)
+      ).to.be.revertedWith('AccessControl: can only renounce roles for self')
+      await hasRole(PAUSER_ROLE, address1.address, true)
+    })
+
+    // Helper functions
+
+    async function hasRole (role, account, expected) {
+      await expect(await cmtat.hasRole(role, account)).to.equal(expected)
+    }
+
+    async function grantRole (caller, role, account) {
+      await expect(await cmtat.connect(caller).grantRole(role, account)).to.emit(cmtat, 'RoleGranted').withArgs(role, account, caller.address)
+    }
+
+    async function renounceRole (caller, role, account) {
+      await expect(await cmtat.connect(caller).renounceRole(role, account)).to.emit(cmtat, 'RoleRevoked').withArgs(role, account, caller.address)
+    }
+    async function revokeRole (caller, role, account) {
+      await expect(await cmtat.connect(caller).revokeRole(role, account)).to.emit(cmtat, 'RoleRevoked').withArgs(role, account, caller.address)
+    }
+  })
+})
