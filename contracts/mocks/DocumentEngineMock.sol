115ac4d2da92221f16e46e539ee522bb32f17d63
Ryan Sauge <71391932+rya-sge@users.noreply.github.com>
2024-11-13 16:39:14 +0100

    Use a struct to represent a document

--- contracts/mocks/DocumentEngineMock.sol
+++ contracts/mocks/DocumentEngineMock.sol
@@ -3,94 +3,82 @@
 pragma solidity ^0.8.20;
 import "../interfaces/engine/draft-IERC1643.sol";
 interface IERC1643Whole is IERC1643{
-
+    /// uri The URI of the document
+    /// @return documentHash The hash of the document contents
+    /// @return lastModified The timestamp of the last modification
+    struct DocumentInfo {
+        string name;
+        string uri;
+        bytes32 documentHash;
+    }
     // Document Management
-    function setDocument(bytes32 _name, string memory _uri, bytes32 _documentHash) external;
-    function removeDocument(bytes32 _name) external;
+    function setDocument(DocumentInfo calldata doc) external;
+    function removeDocument(string memory name) external;
 
     // Document Events
-    event DocumentRemoved(bytes32 indexed _name, string _uri, bytes32 _documentHash);
-    event DocumentUpdated(bytes32 indexed _name, string _uri, bytes32 _documentHash);
+    event DocumentRemoved(string indexed name, Document doc);
+    event DocumentUpdated(string indexed name, Document doc);
 
 }
 /*
 * @title a DocumentEngine mock for testing, not suitable for production
 */
 contract DocumentEngineMock is IERC1643Whole {
-     struct Document {
-        string uri;
-        bytes32 documentHash;
-        uint256 lastModified;
-    }
-
-    mapping(bytes32 => Document) private documents;
-    bytes32[] private documentNames;
+    /*struct DocumentStorage {
+        uint256 key;
+        Document doc;
+    }*/
+    mapping(string => Document) private documents;
+    mapping(string => uint256) private documentKey;
+    string[] private documentNames;
 
     /// @dev Error thrown when a document does not exist
     error DocumentDoesNotExist();
 
     /// @notice Retrieves the document details by name
-    /// @param name_ The name of the document
-    /// @return uri The URI of the document
-    /// @return documentHash The hash of the document contents
-    /// @return lastModified The timestamp of the last modification
-    function getDocument(bytes32 name_)
+    /// @param name The name of the document
+    function getDocument(string memory name)
         external
         view
         override
-        returns (string memory uri, bytes32 documentHash, uint256 lastModified)
+        returns (Document memory doc)
     {
-        if (bytes(documents[name_].uri).length == 0) {
-            return("", 0x0, 0);
-        }
-
-        Document storage doc = documents[name_];
-        return (doc.uri, doc.documentHash, doc.lastModified);
+        return documents[name];
     }
 
     /// @notice Sets or updates a document
-    /// @param name_ The name of the document
-    /// @param uri_ The URI of the document
-    /// @param documentHash_ The hash of the document contents
-    function setDocument(bytes32 name_, string memory uri_, bytes32 documentHash_) external override {
-        Document storage doc = documents[name_];
-        bool isUpdate = bytes(doc.uri).length != 0;
-
-        doc.uri = uri_;
-        doc.documentHash = documentHash_;
+    /// @param doc_ the document
+    function setDocument(DocumentInfo calldata doc_) external override {
+        Document storage doc = documents[doc_.name];
+        doc.uri = doc_.uri;
+        doc.documentHash = doc_.documentHash;
         doc.lastModified = block.timestamp;
-
-        if (!isUpdate) {
-            documentNames.push(name_);
+        if (documentKey[doc_.name] == 0) {
+            // To avoid key == 0
+            uint256 key = documentNames.length + 1;
+            documentKey[doc_.name] = key;
+            documentNames.push(doc_.name);
         }
-
-        emit DocumentUpdated(name_, uri_, documentHash_);
+        emit DocumentUpdated(doc_.name, doc);
     }
 
     /// @notice Removes a document
-    /// @param name_ The name of the document
-    function removeDocument(bytes32 name_) external override {
-        if (bytes(documents[name_].uri).length == 0) {
+    /// @param name The name of the document
+    function removeDocument(string calldata name) external override {
+        if (documentKey[name] == 0) {
             revert DocumentDoesNotExist();
         }
-
-        Document memory doc = documents[name_];
-        delete documents[name_];
-
-        for (uint256 i = 0; i < documentNames.length; i++) {
-            if (documentNames[i] == name_) {
-                documentNames[i] = documentNames[documentNames.length - 1];
-                documentNames.pop();
-                break;
-            }
-        }
-
-        emit DocumentRemoved(name_, doc.uri, doc.documentHash);
+        Document memory doc = documents[name];
+        documentNames[documentKey[name] - 1] = documentNames[documentNames.length - 1];
+        documentNames.pop();
+        delete documents[name];
+        documentKey[name] = 0;
+        emit DocumentRemoved(name, doc);
     }
 
     /// @notice Retrieves all document names
     /// @return An array of document names
-    function getAllDocuments() external view override returns (bytes32[] memory) {
+    function getAllDocuments() external view override returns (string[] memory) {
         return documentNames;
     }
 }
